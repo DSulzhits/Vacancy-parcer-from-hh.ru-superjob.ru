01d671fc03ba84b1234d18f5ec00a2465481a88c
DSulzhits <dsulzhits@gmail.com>
2023-04-01 13:13:26 +0300

    add python key to SuperJob

--- classes/engine_classes.py
+++ classes/engine_classes.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
+from dotenv import load_dotenv
+import os
 import requests
+import json
 
 
 class Engine(ABC):
@@ -14,62 +17,53 @@ class Engine(ABC):
 
 
 class HH(Engine):
+
+    def __init__(self, vacancy):
+        self.vacancy = vacancy
+
     def get_request(self):
         vacancies = []
         vacancies_dicts = []
-        for num in range(1000):
-            self.url = 'https://api.hh.ru/vacancies'
-            params = {'text': 'python', 'per_page': 20, 'page': num}
-            response = requests.get(self.url, params=params)
+        for num in range(1):  # при значении 50 выбирает 1000 вакансий
+            url = 'https://api.hh.ru/vacancies'
+            params = {'text': {self.vacancy}, "areas": 113, 'per_page': 20, 'page': num}
+            response = requests.get(url, params=params)
             info = response.json()
-            if 'items' not in info:
-                continue
+            if info is None:
+                return "Данные не получены"
+            elif 'errors' in info:
+                return info['errors'][0]['value']
+            elif 'items' not in info:
+                return "Нет вакансий"
             else:
                 for vacancy in range(20):
                     vacancies.append([info['items'][vacancy]['name'], info['items'][vacancy]['apply_alternate_url'],
                                       info['items'][vacancy]['snippet']['requirement'],
                                       info['items'][vacancy]['salary']])
         for vacancy in vacancies:
-            vacancy_dict = {}
-            vacancy_dict['name'] = vacancy[0]
-            vacancy_dict['url'] = vacancy[1]
-            vacancy_dict['requirementrl'] = vacancy[2]
-            vacancy_dict['salary'] = vacancy[3]
+            vacancy_dict = {'name': vacancy[0], 'url': vacancy[1], 'requirement': vacancy[2], 'salary': vacancy[3]}
             vacancies_dicts.append(vacancy_dict)
 
+        with open('hh_ru.json', 'w', encoding='UTF-8') as file:
+            json.dump(vacancies_dicts, file, indent=2, ensure_ascii=False)
+
         return vacancies_dicts
-
-
-# class HH(Engine):
-#     def get_request(self):
-#         items = []
-#
-#         i: int
-#         for i in range(1000):
-#             url = "https://api.hh.ru/vacancies"
-#             par = {
-#                 'text': 'python',
-#                 'areas': 113,
-#                 'page': i
-#             }
-#             self.request = requests.get(url, params=par).json()
-#             if self.request == []:
-#                 continue
-#             for y in range(20):
-#                 items.append(self.request['items'][y]['name'])
-#                 items.append(self.request['items'][y]['alternate_url'])
-#                 items.append(self.request['items'][y]['snippet']['requirement'])
-#                 items.append(self.request['items'][y]['salary'])
-#         return items
-
-
+        # return info
 
 
 class SuperJob(Engine):
+
+    def __init__(self, vacancy):
+        self.vacancy = vacancy
+        load_dotenv()
+        self.api_key = os.getenv('SJ_API_KEY', 'key_error')
+
     def get_request(self):
         pass
 
 
-hh = HH()
-print(hh.get_request())
-print(len(hh.get_request()))
+# hh = HH("Python разработчик")
+# print(hh.get_request())
+sj = SuperJob("Python разработчик")
+print(sj.api_key)
+print(os.getenv("SJ_API_KEY"))
