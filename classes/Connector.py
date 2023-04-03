d1830d06b8b0802c8fab2eee899f4dc7ffa120ec
DSulzhits <dsulzhits@gmail.com>
2023-04-03 14:10:51 +0300

    Написан Connector.py

--- classes/Connector.py
+++ classes/Connector.py
@@ -1,27 +1,35 @@
+import json
+
+
 class Connector:
     """
     Класс коннектор к файлу, обязательно файл должен быть в json формате
     не забывать проверять целостность данных, что файл с данными не подвергся
     внешнего деградации
     """
-    __data_file = None
-    @property
-    def data_file(self):
-        pass
 
-    @data_file.setter
-    def data_file(self, value):
-        # тут должен быть код для установки файла
-        self.__connect()
+    # __data_file = None
+    # @property
+    # def data_file(self):
+    #     pass
+    #
+    # @data_file.setter
+    # def data_file(self, value):
+    #     # тут должен быть код для установки файла
+    #     self.__connect()
+    #
+    # def __connect(self):
+    #     """
+    #     Проверка на существование файла с данными и
+    #     создание его при необходимости
+    #     Также проверить на деградацию и возбудить исключение
+    #     если файл потерял актуальность в структуре данных
+    #     """
+    #     pass
 
-    def __connect(self):
-        """
-        Проверка на существование файла с данными и
-        создание его при необходимости
-        Также проверить на деградацию и возбудить исключение
-        если файл потерял актуальность в структуре данных
-        """
-        pass
+    # def __init__(self, HH, SuperJob):
+    #     self.HH = HH
+    #     self.SuperJob = SuperJob
 
     def insert(self, data):
         """
@@ -29,7 +37,7 @@ class Connector:
         """
         pass
 
-    def select(self, query):
+    def select(self):
         """
         Выбор данных из файла с применением фильтрации
         query содержит словарь, в котором ключ это поле для
@@ -37,7 +45,22 @@ class Connector:
         {'price': 1000}, должно отфильтровать данные по полю price
         и вернуть все строки, в которых цена 1000
         """
-        pass
+
+        HH_info = []
+        SJ_info = []
+        with open("hh_ru.json", encoding='utf-8') as file:
+            vacancies = json.load(file)
+            for vacancy in vacancies:
+                HH_info.append([vacancy['name'], vacancy['url'], vacancy['requirement'], vacancy['salary_from'],
+                                vacancy['salary_to']])
+
+        with open("sj_ru.json", encoding='utf-8') as file:
+            vacancies = json.load(file)
+            for vacancy in vacancies:
+                SJ_info.append([vacancy['name'], vacancy['url'], vacancy['requirement'], vacancy['salary_from'],
+                                vacancy['salary_to']])
+
+        return HH_info, SJ_info
 
     def delete(self, query):
         """
@@ -48,15 +71,18 @@ class Connector:
         pass
 
 
-if __name__ == '__main__':
-    df = Connector('df.json')
+# if __name__ == '__main__':
+#     df = Connector('df.json')
+#
+#     data_for_file = {'id': 1, 'title': 'tet'}
+#
+#     df.insert(data_for_file)
+#     data_from_file = df.select(dict())
+#     assert data_from_file == [data_for_file]
+#
+#     df.delete({'id':1})
+#     data_from_file = df.select(dict())
+#     assert data_from_file == []
 
-    data_for_file = {'id': 1, 'title': 'tet'}
-
-    df.insert(data_for_file)
-    data_from_file = df.select(dict())
-    assert data_from_file == [data_for_file]
-
-    df.delete({'id':1})
-    data_from_file = df.select(dict())
-    assert data_from_file == []
+con = Connector()
+print(con.select())
