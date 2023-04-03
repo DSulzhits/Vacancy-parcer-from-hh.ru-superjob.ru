2e5f64ee659ef423fcf0ec4d48ca714e915ffe6a
DSulzhits <dsulzhits@gmail.com>
2023-04-03 19:40:14 +0300

    Написана часть основного кода

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+from classes.Connector import Connector
+from classes.jobs_classes import Vacancy, HHVacancy, SJVacancy, sorting, get_top
+from classes.engine_classes import HH, SuperJob
+
+
 def main():
-    pass
+    user_input = input("""На каком ресурсе хотите осуществить поиск:
+    HeadHunter нажмите 1
+    SuperJob нажмите 2\n""").lower()
+    user_prof = input("""Введите желаемую специальность\n""")
+    if user_input == "1":
+        hh = HH(user_prof)
+        hh.get_request()
+        con = Connector()
+        con.connectHH()
+        con_hh = con.select_HH()
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
