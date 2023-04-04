b44be140ba5e40412f8ff09e001c65dc52f06681
DSulzhits <dsulzhits@gmail.com>
2023-04-04 15:38:41 +0300

    Изменены классы HH, и SuperJob для взаимодействия с переписанным Connector, а также для проверки обращения

--- classes/engine_classes.py
+++ classes/engine_classes.py
@@ -17,19 +17,21 @@ class Engine(ABC):
 
 
 class HH(Engine):
+    vacancies_all = []
+    vacancies = []
+    vacancies_dicts = []
 
     def __init__(self, vacancy):
         self.vacancy = vacancy
 
     def get_request(self):
-        vacancies = []
-        vacancies_dicts = []
-        for num in range(1):  # при значении 50 выбирает 1000 вакансий
+
+        for num in range(
+                50):  # при значении 50 выбирает из 1000 вакансий те в которых есть информация о З/П и она в RUR
             url = 'https://api.hh.ru/vacancies'
             params = {'text': {self.vacancy}, "areas": 113, 'per_page': 20, 'page': num}
             response = requests.get(url, params=params)
             info = response.json()
-
             if info is None:
                 return "Данные не получены"
             elif 'errors' in info:
@@ -38,30 +40,34 @@ class HH(Engine):
                 return "Нет вакансий"
             else:
                 for vacancy in range(20):
+                    self.vacancies_all.append(vacancy)
                     if info['items'][vacancy]['salary'] is not None \
                             and info['items'][vacancy]['salary']['currency'] == "RUR":
-                        vacancies.append([info['items'][vacancy]['employer']['name'],
-                                          info['items'][vacancy]['name'], info['items'][vacancy]['apply_alternate_url'],
-                                          info['items'][vacancy]['snippet']['requirement'],
-                                          info['items'][vacancy]['salary']['from'],
-                                          info['items'][vacancy]['salary']['to']])
-        for vacancy in vacancies:
+                        self.vacancies.append([info['items'][vacancy]['employer']['name'],
+                                               info['items'][vacancy]['name'],
+                                               info['items'][vacancy]['apply_alternate_url'],
+                                               info['items'][vacancy]['snippet']['requirement'],
+                                               info['items'][vacancy]['salary']['from'],
+                                               info['items'][vacancy]['salary']['to']])
+        for vacancy in self.vacancies:
             vacancy_dict = {'employer': vacancy[0], 'name': vacancy[1], 'url': vacancy[2], 'requirement': vacancy[3],
                             'salary_from': vacancy[4], 'salary_to': vacancy[5]}
             if vacancy_dict['salary_from'] is None:
                 vacancy_dict['salary_from'] = 0
             elif vacancy_dict['salary_to'] is None:
                 vacancy_dict['salary_to'] = vacancy_dict['salary_from']
-            vacancies_dicts.append(vacancy_dict)
+            self.vacancies_dicts.append(vacancy_dict)
 
-        with open('hh_ru.json', 'w', encoding='UTF-8') as file:
-            json.dump(vacancies_dicts, file, indent=2, ensure_ascii=False)
-        return vacancies_dicts
-
-        # return info
+        with open(f'{self.vacancy}_hh_ru.json', 'w', encoding='UTF-8') as file:
+            json.dump(self.vacancies_dicts, file, indent=2, ensure_ascii=False)
+        print(f"Отбор осуществляется из {len(self.vacancies_all)} вакансий (проверка обращения к сервису)")
+        return self.vacancies_dicts
 
 
 class SuperJob(Engine):
+    vacancies_all = []
+    vacancies = []
+    vacancies_dicts = []
 
     def __init__(self, vacancy):
         self.vacancy = vacancy
@@ -69,36 +75,43 @@ class SuperJob(Engine):
         self.api_key = os.getenv('SJ_API_KEY', 'key_error')
 
     def get_request(self):
-        vacancies = []
-        vacancies_dicts = []
         url = 'https://api.superjob.ru/2.0/vacancies/'
-        for num in range(10):
+        for num in range(500):
             headers = {
                 "X-Api-App-Id": self.api_key
             }
             params = {
                 'keywords': self.vacancy,
                 'page': num,
-                'count': 5
+                'count': 1
             }
             response = requests.get(url, headers=headers, params=params)
             info = response.json()
+            self.vacancies_all.append(info)
             if info['objects'] is None:
                 continue
             if info['objects'][0]['payment_from'] is not None and info['objects'][0]['currency'] == "rub":
-                vacancies.append(
+                self.vacancies.append(
                     [info['objects'][0]['client']['title'], info['objects'][0]['profession'],
                      info['objects'][0]['link'], info['objects'][0]['candidat'],
                      info['objects'][0]['payment_from'], info['objects'][0]['payment_to']])
 
-        for vacancy in vacancies:
+        for vacancy in self.vacancies:
             vacancy_dict = {'employer': vacancy[0], 'name': vacancy[1], 'url': vacancy[2], 'requirement': vacancy[3],
                             'salary_from': vacancy[4], 'salary_to': vacancy[5]}
             if vacancy_dict['salary_from'] is None:
                 vacancy_dict['salary_from'] = 0
             elif vacancy_dict['salary_to'] == 0:
                 vacancy_dict['salary_to'] = vacancy_dict['salary_from']
-            vacancies_dicts.append(vacancy_dict)
+            self.vacancies_dicts.append(vacancy_dict)
 
-        with open('sj_ru.json', 'w', encoding='UTF-8') as file:
-            json.dump(vacancies_dicts, file, indent=2, ensure_ascii=False)
+        with open(f'{self.vacancy}_sj_ru.json', 'w', encoding='UTF-8') as file:
+            json.dump(self.vacancies_dicts, file, indent=2, ensure_ascii=False)
+        print(f"Отбор осуществляется из {len(self.vacancies_all)} вакансий (проверка обращения к сервису)")
+        return self.vacancies_dicts
+
+
+# sj = SuperJob("python")
+# sj.get_request()
+# print(sj.vacancies_all)
+# print(len(sj.vacancies_all))
